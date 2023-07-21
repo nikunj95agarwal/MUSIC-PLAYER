73d23912f23a7759235f362a71bfaf898d52574a
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-07-21 14:08:41 +0530

    Add files via upload
    TESTING

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const preloader = document.querySelector('.preloader');
+  
+    setTimeout(function () {
+      preloader.style.display = 'none';
+    }, 2000); // 2000 milliseconds = 2 seconds
+  });
+
+
+var audio1 = document.querySelector('audio') 
+  var play_btn = document.getElementById("play_btn");
+play_btn.addEventListener("click", function() {
+  if (play_btn.classList.contains("fa-play")) {
+    play_btn.classList.remove("fa-play");
+   play_btn.classList.add("fa-pause");
+   play_btn.style.paddingLeft = "0.2rem"; 
+audio1.play();
+
+  } else { 
+    play_btn.classList.remove("fa-pause");
+    play_btn.classList.add("fa-play");
+    audio1.pause();
+  }
+});
